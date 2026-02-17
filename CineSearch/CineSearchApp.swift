24abff011c7c0a18ccbbb1e844b2cc0e97c50e01
Dev-MuTTiNeeR <cemakkaya1312@gmail.com>
2026-02-17 17:14:00 -0300

    Initial Commit

---
+++ CineSearch/CineSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineSearchApp.swift
+//  CineSearch
+//
+//  Created by Cem Akkaya on 17/02/26.
+//
+
+import SwiftUI
+
+@main
+struct CineSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CineSearch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CineSearch
+//
+//  Created by Cem Akkaya on 17/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
